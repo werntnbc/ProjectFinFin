254274ca0000bb38e7023cbc9d8d2446781069a6
TNBC's <w.wernwern@hotmail.com>
2015-06-25 01:08:08 +0700

    login+signup+newsfeed

--- app/src/main/java/com/projectfinfin/projectfinfin/NewsfeedActivity.java
+++ app/src/main/java/com/projectfinfin/projectfinfin/NewsfeedActivity.java
@@ -1,23 +1,87 @@
 package com.projectfinfin.projectfinfin;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
+import android.content.Context;
+//import android.support.v7.app.ActionBarActivity;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
+public class NewsfeedActivity extends ListActivity {
 
-public class NewsfeedActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsfeed);
+        setupCustomListView();
     }
 
+    private void setupCustomListView() {
+        setListAdapter(new EfficientAdapter(getApplicationContext()));
+    }
+
+    public class EfficientAdapter extends BaseAdapter{
+
+        public Context mContext;
+        public LayoutInflater mInflater;
+
+        public EfficientAdapter(Context ctx){
+            mContext = ctx;
+            mInflater = LayoutInflater.from(mContext);
+        }
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public View getView(int position, View convertedView, ViewGroup viewGroup) {
+
+            ViewHolder viewHolder = null;
+            if(convertedView == null){
+                //create convertedView and bind Widgets in convertedView
+                convertedView = mInflater.inflate(R.layout.listview_row , null);
+                viewHolder = new ViewHolder();
+                viewHolder.brandImage = (ImageView)convertedView.findViewById(R.id.brandImage);
+                viewHolder.titleTextView = (TextView)convertedView.findViewById(R.id.titleTextView);
+                convertedView.setTag(viewHolder);
+            }else{
+                // re-bind widgets in convertedView
+                viewHolder = (ViewHolder) convertedView.getTag();
+            }
+                //update content in convertedView
+         //   viewHolder.titleTextView.setText(String.valueOf(position));
+            return convertedView;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+    }
+
+        public class ViewHolder{
+            public ImageView brandImage;
+            public TextView titleTextView;
+        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+       // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_newsfeed, menu);
         return true;
     }
